125d893e707e804c632d7fdbfea6c1a80e95b080
Suraj <22918434+suraj143@users.noreply.github.com>
2021-05-19 17:22:52 +0530

    updated code

--- automate.bat
+++ automate.bat
@@ -12,12 +12,11 @@ rem choco uninstall azure-cli -y
 powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi"
 choco install kubernetes-cli -y
 kubectl --version
-start /b az --version 
 choco install googlechrome -y
 choco install notepadplusplus -y
 
 echo "sleep for 10 sec"
-powershell Start-Sleep -s 10
+rem powershell Start-Sleep -s 10
 
 set "clientId=c8cc3204-f015-495f-82d6-e2b0ecefa7e3"
 set "clientSecret=mj-ovxpilLJ.GgNX_s-idkJflhJK~Y0hwy" 
@@ -29,7 +28,7 @@ echo "Azure login"
 start /b az login --service-principal -u %clientId% -p %clientSecret% --tenant %tenantId%
 
 echo "sleep for 10 sec"
-powershell Start-Sleep -s 10
+rem powershell Start-Sleep -s 10
 
 echo "Connect to AKS"
 start /b az aks get-credentials --resource-group %resoucegroup% --name %clustername%
@@ -41,19 +40,19 @@ echo "Delete core in AKS"
 kubectl delete -f https://raw.githubusercontent.com/suraj143/netapptest/main/core.yaml
 
 echo "sleep for 20 sec"
-powershell Start-Sleep -s 20
+rem powershell Start-Sleep -s 20
 
 echo "Deploy core in AKS"
-kubectl apply -f https://raw.githubusercontent.com/suraj143/netapptest/main/core.yaml
+rem kubectl apply -f https://raw.githubusercontent.com/suraj143/netapptest/main/core.yaml
 
 echo "sleep for 60 sec"
-powershell Start-Sleep -s 60
+rem powershell Start-Sleep -s 60
 
 echo "List running pods in core-dev namespace"
 kubectl get pods -n core-dev
 
 echo "sleep 30 sec"
-powershell Start-Sleep -s 30
+rem powershell Start-Sleep -s 30
 
 echo "List pod loadbalencer endpoints in core-dev namespace"
 kubectl get svc -n core-dev
